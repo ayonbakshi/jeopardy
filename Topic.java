225af15d4ef21e00cfa730c78478099d6f238a28
Megan Niu <megan.niu@gmail.com>
2016-11-21 14:07:31 -0500

    Topic class without comments

---
+++ Topic.java
@@ -0,0 +1,31 @@
+public class Topic {
+    private String topic;
+    private Question[] questions;
+    private boolean[] chosen;
+    
+    /**
+     * 
+     * @param questions
+     * @param answers
+     * @param correct
+     * @param dollars
+     * @param topic 
+     */
+    public Topic(String[] questions, String[][] answers, int[]correct, int[]dollars, String topic){
+        this.topic = topic;
+        for (int i = 0; i < questions.length; i++){
+            Question[i] = new Question(dollars[i], correct[i], questions[i], answers[i]);
+        }
+        chosen = new boolean[5];
+    }
+    
+    public Topic(Question[] questions, String topic){
+        this.topic = topic;
+        this.questions = questions;
+        chosen = new boolean[5];
+    }
+    
+    public Question[] getQuestion(int questionNum){
+        return questions;
+    }
+}
