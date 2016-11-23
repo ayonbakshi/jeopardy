7d354a0d7c25cd461c66cbd583a880d3d294dd40
wecassidy <tungsten314@gmail.com>
2016-11-23 12:42:29 -0500

    Read questions from a tab-separated CSV file.
    Format:
    Question	Topic	Value	Correct	Answer 0	Answer 1	Answer 2	Answer 3	Answer 4	Answer 5

--- Jeopardy/src/Jeopardy.java
+++ Jeopardy/src/Jeopardy.java
@@ -1,10 +1,41 @@
-import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Jeopardy extends JFrame {
   private Player[] players;
   
-  public Jeopardy() {
+  public Jeopardy() throws FileNotFoundException {
+    super("Jeopardy!");
+
+    // Read questions
+    Scanner qScan;
+    try {
+      qScan = new Scanner(new File("data" + File.separator + "questions.csv"));
+    } catch (FileNotFoundException fnfe) { // Can't find quesiton file
+      JOptionPane.showMessageDialog(null, "Can't find question file.", "Error", JOptionPane.ERROR_MESSAGE);
+      throw fnfe;
+    }
+    
+    List<String> questions = new ArrayList<String>();
+    List<String> topics = new ArrayList<String>();
+    List<Integer> values = new ArrayList<Integer>();
+    List<Integer> correct = new ArrayList<Integer>();
+    List<String[]> answers = new ArrayList<String[]>();
+
+    while (qScan.hasNextLine()) {
+      String[] dataRow = qScan.nextLine().split("\t");
+      questions.add(dataRow[0]);
+      topics.add(dataRow[1]);
+      values.add(Integer.parseInt(dataRow[2]));
+      correct.add(Integer.parseInt(dataRow[3]));
+      String[] answerArray = {dataRow[4], dataRow[5], dataRow[6], dataRow[7], dataRow[8], dataRow[9]};
+      answers.add(answerArray);
+    }
+    
     this.players = new Player[3];
     
     for (int i = 0; i < 3; i++) {
@@ -18,7 +49,7 @@ public class Jeopardy extends JFrame {
     }
   }
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     Jeopardy game = new Jeopardy();
   }
 }
