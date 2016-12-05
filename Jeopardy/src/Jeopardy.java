6e8079eebeb0645d8976cd248bbf05eb3677c6f1
wecassidy <tungsten314@gmail.com>
2016-12-04 19:00:27 -0500

    Replace the title text with an image using (approximately) the font from the actual Jeopardy! logo.

--- Jeopardy/src/Jeopardy.java
+++ Jeopardy/src/Jeopardy.java
@@ -224,13 +224,12 @@ public class Jeopardy extends JPanel implements ActionListener {
     // Set up GUI
     GridBagConstraints gbc;
     // Title
-    JLabel title = new JLabel("Jeopardy!");
-    title.setFont(GameUtils.TITLE_FONT);
-
+    JLabel title = new JLabel(new ImageIcon(GameUtils.findImage("title.png")));
     gbc = new GridBagConstraints();
     gbc.gridx = 0;
     gbc.gridy = 0;
     gbc.gridwidth = 3;
+    gbc.insets = new Insets(10, 0, 0, 0);
     gbc.anchor = GridBagConstraints.CENTER; // Centre the title
     this.add(title, gbc);
 
